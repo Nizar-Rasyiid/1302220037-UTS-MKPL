bd2e1622835ac4883217c1844bcd382f3e36010f
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2025-04-17 00:00:54 +0700

    Membuat TaxProfile untuk mengisi informasi Pembayar Pajak,dan menghindari Long param dan Switch statements

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,44 +1,19 @@
-package lib;
+	package lib;
 
-public class TaxFunction {
+	public class TaxFunction {
+		private static final int PERSONAL_ALLOWANCE = 54_000_000;
+		private static final int MARRIED_ALLOWANCE = 4_500_000;
+		private static final int CHILD_ALLOWANCE = 1_500_000;
+		private static final double TAX_RATE = 0.05;
+		private TaxProfile profileofile;
+		public static int calculateTax(TaxProfile profile) {
 
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
+			int totalIncome = (profile.getMonthlySalary() + profile.getOtherMonthlyIncome()) * profile.getNumberOfMonthWorking();
+			int ptkp = calculatePTKP(profile.isMarried(), profile.getNumberOfChildren());
+			int taxableIncome = totalIncome - profile.getDeductible() - ptkp;
+
+			int tax = (int) Math.round(TAX_RATE * taxableIncome);
+			return Math.max(tax, 0);
 		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+
 	}
-	
-}
