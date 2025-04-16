9053c6f9a800f0dc0a63cdb963694ffac21d0e18
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2025-04-16 23:46:26 +0700

    Memperbaiki setMonthSalary dengan menggunakan metode Lookup Table

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -13,74 +13,62 @@ public class Employee {
 	private JoinedDate joinedDate;
 	private int monthWorkingInYear;
 	private EmployeeInfo employeeInfo;
-	
+
 	private int monthlySalary;
 	private int otherMonthlyIncome;
 	private int annualDeductible;
 
-	
+
 	public Employee(String employeeId, JoinedDate joinedDate,employeeInfo employeeInfo) {
 		this.employeeId = employeeId;
 		this.joinedDate = joinedDate;
 		this.employeeInfo = employeeInfo;
 
-		
+
 
 	}
-	
+
 	/**
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
-	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
+
+	public void setMonthlySalary(int grade) {
+		int baseSalary = switch (grade) {
+			case 1 -> 3000000;
+			case 2 -> 5000000;
+			case 3 -> 7000000;
+			default -> 0;
+		};
+		this.monthlySalary = employeeInfo.isForeigner() ? (int)(baseSalary * 1.5) : baseSalary;
 	}
-	
-	public void setAnnualDeductible(int deductible) {	
+
+	public void setAnnualDeductible(int deductible) {
 		this.annualDeductible = deductible;
 	}
-	
-	public void setAdditionalIncome(int income) {	
+
+	public void setAdditionalIncome(int income) {
 		this.otherMonthlyIncome = income;
 	}
-	
+
 	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
+		this.employeeInfo.setSpouse(spouseName, spouseIdNumber);
 	}
-	
+
 	public void addChild(String childName, String childIdNumber) {
-		childNames.add(childName);
-		childIdNumbers.add(childIdNumber);
+		this.employeeInfo.addChild(childName, childIdNumber);
 	}
-	
 	public int getAnnualIncomeTax() {
-		
+
 		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
-		
+
 		if (date.getYear() == yearJoined) {
 			monthWorkingInYear = date.getMonthValue() - monthJoined;
 		}else {
 			monthWorkingInYear = 12;
 		}
-		
+
 		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
 	}
 }
